df309f96ac8ae0dcc30fe3e55f43cab6247c12e8
rakesh <rakesh.patel2@sky.com>
2017-01-29 20:05:51 +0000

    added BooksRepository implementation
    Former-commit-id: ae0e2c9dad46141cc54cc5c2d3f52d205acd0271

---
+++ app/src/main/java/com/om/snipit/repositories/impl/DatabaseBooksRepository.java
@@ -0,0 +1,33 @@
+package com.om.snipit.repositories.impl;
+
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.om.snipit.classes.DatabaseHelper;
+import com.om.snipit.models.Book;
+import com.om.snipit.repositories.BooksRepository;
+
+import android.content.Context;
+
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.List;
+
+
+public class DatabaseBooksRepository implements BooksRepository {
+
+    private final DatabaseHelper databaseHelper;
+
+    public DatabaseBooksRepository(Context context) {
+        databaseHelper = OpenHelperManager.getHelper(context, DatabaseHelper.class);
+    }
+
+    @Override
+    public List<Book> getBooks() {
+        try {
+            return databaseHelper.getBookDAO().queryBuilder()
+                    .orderBy("order", true)
+                    .query();
+        } catch (SQLException e) {
+            return Collections.emptyList();
+        }
+    }
+}
